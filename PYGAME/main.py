a3f6bceb3e50b7cdde9ac51d7b7c0bbaf8886124
michu <skupinski420@outlook.com>
2023-12-19 03:23:24 +0100

    ls

--- PYGAME/main.py
+++ PYGAME/main.py
@@ -10,31 +10,60 @@ height = 480
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Light Souls")
 
-def background():
-    background = pygame.image.load("bg3.png").convert_alpha()
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
-    clock.tick(60)
+# more settings
+# player in the middle
+player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+red = (255, 0, 0)
+rect = pygame.Rect(50, 60, 200, 80) # x, y, width, height
 
-# main game loop function
+# generate background 
+def background():
+    background = pygame.image.load("bg3.png").convert_alpha() # alpha for png images for transparency
+    screen.blit(background, (0, 0)) # attach bg
+    pygame.display.flip()
+    clock.tick(60) # 60 fps max
+
+def player():
+    pygame.draw.rect(screen, red, rect)
+
+# main menu 
+def main_menu():
+    while running == True:
+        for event in pygame.event.get():
+            # QUIT event - x clicked on window
+            if event.type == pygame.QUIT:
+                running = False
+                break
+
+# main game
+def main_game():
+    while running == True:
+        for event in pygame.event.get():
+            # QUIT event - x clicked on window
+            if event.type == pygame.QUIT:
+                running = False
+                break
+
+# game loop function
 def main():
     running = True
 
     while running == True:
         # check for all occuring events like key presses, mouse moved etc
         for event in pygame.event.get():
-
             # QUIT event - x clicked on window
             if event.type == pygame.QUIT:
                 running = False
                 break
 
-        background()
-        
+        # background()
+        screen.fill("blue")
+        player()
+        pygame.display.flip()
 
 
     # if loop ends, quit game
-    pygame.QUIT
+    pygame.quit()
 
 # check if python file is run directly, and not imported etc
 if __name__ == "__main__":
