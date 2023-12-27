ae413f244d811ba0252f0b6c2e8b6393df656547
michu <skupinski420@outlook.com>
2023-12-27 23:13:35 +0100

    some ctf

---
+++ CTF/aHead.py
@@ -0,0 +1,9 @@
+# request to web
+# get method
+# curl http://mercury.picoctf.net:21939/index.php
+
+# post method
+# curl -X POST http://mercury.picoctf.net:21939/index.php
+
+# head method
+# curl -I HEAD http://mercury.picoctf.net:21939/index.php
---
+++ CTF/insp3ct0r.py
@@ -0,0 +1,5 @@
+
+# 1/3 found in 41511 (html source) picoCTF{tru3_d3
+# 2/3 found in mycss.css t3ct1ve_0r_ju5t
+# 3/3 found in sources/myjs.js _lucky?832b0699}
+# flag picoCTF{tru3_d3t3ct1ve_0r_ju5t_lucky?832b0699}
